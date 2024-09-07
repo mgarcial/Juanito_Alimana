5840031402b2e8fb1ee5646f761feee61a8e3049
Kenny <82070420+KennyM14@users.noreply.github.com>
2024-09-07 02:00:50 -0500

    Level Menu

---
+++ Assets/Scripts/UI/MenuEvent.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuEvent : MonoBehaviour
+{
+
+    public void loadLevel()
+    {
+        SceneManager.LoadScene("SpawnTest"); 
+    }
+
+}

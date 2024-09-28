738f4eed579d6b071a682e0749229c68965d02c6
Kenny <82070420+KennyM14@users.noreply.github.com>
2024-09-28 17:02:05 -0500

    Dialogue System

--- Assets/Scripts/Collider/FungusCollider.cs
+++ Assets/Scripts/Collider/FungusCollider.cs
@@ -2,19 +2,27 @@ using Fungus;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Fungus; 
 
 public class FungusCollider : MonoBehaviour
 {
     public Flowchart flowChart;
-    public string blockName; 
+    public string blockName;
+    private bool hasActivated = false; 
 
     public void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if(other.CompareTag("Player") && !hasActivated)
         {
             flowChart.ExecuteBlock(blockName);
+            hasActivated = true;
         }
     }
 
+    private void Update()
+    {
+        if(!flowChart.HasExecutingBlocks())
+        {
+
+        }
+    }
 }

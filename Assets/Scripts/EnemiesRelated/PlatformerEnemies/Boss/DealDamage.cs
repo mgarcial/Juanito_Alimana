14e3b87dd682ee3d2d1100213ecc5b044ddc9bfd
José David Serna <jdserna00@gmail.com>
2024-11-07 20:14:42 -0500

    fixes

--- Assets/Scripts/EnemiesRelated/PlatformerEnemies/Boss/DealDamage.cs
+++ Assets/Scripts/EnemiesRelated/PlatformerEnemies/Boss/DealDamage.cs
@@ -6,13 +6,13 @@ public class DealDamage : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-       /* IDamageable player = collision.GetComponent<IDamageable>();
+        IDamageable player = collision.GetComponent<IDamageable>();
         PlayerController playerInvul = collision.GetComponent<PlayerController>();
         if (player != null && !playerInvul.isInvulnerable)
         {
             Debug.Log("hi");
             player.TakeHit(5);
-        }*/
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
